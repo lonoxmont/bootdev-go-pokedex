9986d8adc666e06fe2ae40ca905b6a9a9576b072
Lonox Mont <lonoxmont@suptg.com>
2025-10-22 15:30:12 -0700

    added catch command

--- repl.go
+++ repl.go
@@ -13,6 +13,7 @@ type config struct {
 	pokeapiClient    pokeapi.Client
 	nextLocationsURL *string
 	prevLocationsURL *string
+	caughtPokemon    map[string]pokeapi.Pokemon
 }
 
 func startRepl(cfg *config) {
@@ -66,6 +67,11 @@ func getCommands() map[string]cliCommand {
 			name:        "help",
 			description: "Displays a help message",
 			callback:    commandHelp,
+		},
+		"catch": {
+			name:        "catch <pokemon>",
+			description: "Try to catch the supplied Pokemon",
+			callback:    commandCatch,
 		},
 		"explore": {
 			name:        "explore <location_name>",
